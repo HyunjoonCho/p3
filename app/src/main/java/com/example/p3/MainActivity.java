2b321a5b2bd75dc53af0ed0dd89e7d795266b3ca
Woo <wtrr59@naver.com>
2019-12-28 12:12:17 +0900

    mergeend

--- app/src/main/java/com/example/p3/MainActivity.java
+++ app/src/main/java/com/example/p3/MainActivity.java
@@ -1,43 +1,102 @@
 package com.example.p3;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import android.graphics.drawable.Drawable;
+import android.Manifest;
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.Toast;
 
-import java.util.ArrayList;
-
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    RecyclerView mRecyclerView = null ;
-    RecyclerAdapter mAdapter = null ;
-    ArrayList<RecyclerItem> mList = new ArrayList<RecyclerItem>();
+
+    Button tab1;
+    Button tab2;
+    Button tab3;
+    String[] permission_list = {
+            Manifest.permission.WRITE_CONTACTS,
+            Manifest.permission.READ_CONTACTS
+    };
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.content_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
 
-        addItem(getDrawable(R.drawable.water));
-        addItem(getDrawable(R.drawable.fall));
-        addItem(getDrawable(R.drawable.image1));
-        addItem(getDrawable(R.drawable.image2));
-        addItem(getDrawable(R.drawable.image3));
+        checkPermission();
 
+        tab1 = findViewById(R.id.main_tab1);
+        tab1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), TabOneActivity.class);
+                ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this,tab1,"custom_transition");
+                startActivity(intent, options.toBundle());
+            }
+        });
 
-        mRecyclerView = findViewById(R.id.recycler1);
-        mAdapter = new RecyclerAdapter(mList);
-        mRecyclerView.setAdapter(mAdapter);
-        mRecyclerView.setLayoutManager(new GridLayoutManager(this, 4));
-        mRecyclerView.addItemDecoration(new MovieItemDecoration(this));
-        mAdapter.notifyDataSetChanged();
+        tab2 = findViewById(R.id.main_tab2);
+        tab2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Gallery.class);
+                ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this,tab2,"custom_transition2");
+                startActivity(intent, options.toBundle());
+            }
+        });
+/*
+        findViewById(R.id.main_tab3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), TabOneActivity.class);
+                startActivity(intent);
+            }
+        });
+
+         */
     }
 
-    public void addItem(Drawable icon){
-        RecyclerItem item = new RecyclerItem();
-        item.setIcon(icon);
-        mList.add(item);
+    public void checkPermission(){
+        //현재 안드로이드 버전이 6.0미만이면 메서드를 종료한다.
+        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.M)
+            return;
+
+        for(String permission : permission_list){
+            //권한 허용 여부를 확인한다.
+            int chk = checkCallingOrSelfPermission(permission);
+
+            if(chk == PackageManager.PERMISSION_DENIED){
+                //권한 허용을여부를 확인하는 창을 띄운다
+                requestPermissions(permission_list,0);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode==0)
+        {
+            for(int i=0; i<grantResults.length; i++)
+            {
+                //허용됬다면
+                if(grantResults[i]== PackageManager.PERMISSION_GRANTED){
+                }
+                else {
+                    Toast.makeText(getApplicationContext(),"앱권한설정하세요", Toast.LENGTH_LONG).show();
+                    finish();
+                }
+            }
+        }
     }
 }
